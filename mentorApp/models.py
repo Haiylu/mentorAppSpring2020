d2b0a0e82683f1f8c81973225c1fdbc7662a1d32
Haiylu <Haiylu.Kibret@gmail.com>
2020-04-17 14:22:11 -0400

    fist commit

---
+++ mentorApp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# 
+# Create your models here.
+# SQlit will be used to create the models
+class mentorItem(models.Model):
+    content=models.TextField()
---
+++ mentorApp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import mentorItem
+
+# Create your views here.
+#def mentorAppView(request):
+       #return render(request, 'mentorApp.html',
+           # {'all_items':all_mentor_items})      
+#<form action="/addMentorApp/" method="post">{% csrf_token %}
+   # <input type="text" name="content"/>
+ #  # <input type="submit" value="Add"/>
+#</form>
